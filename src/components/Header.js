8bbc0d4f1e2311373dc15c35df9eec0f4077c1c5
Louis Weiser <louis.weiser98@gmail.com>
2023-09-22 18:11:58 +0200

    create header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image.js";
+
+import styles from "./Header.module.css";
 
 export default function Header() {
+  const [isMenuVisible, setMenuVisible] = useState(false);
+
+  const onClickHandler = () => {
+    setMenuVisible(!isMenuVisible);
+  };
+
   return (
-    <main>
-      <h1>Hello-Project</h1>
-    </main>
+    <>
+      <header className={styles.header}>
+        <h1>zentaurus-logo</h1>
+        <button className={styles.button} onClick={onClickHandler}>
+          <Image
+            src="/menu.svg"
+            alt="open menu"
+            className={styles.menuLogo}
+            width={100}
+            height={24}
+            priority
+          ></Image>
+        </button>
+      </header>
+      <div className={`${styles.menu} ${isMenuVisible ? styles.visible : ""}`}>
+        <button className={styles.closeMenuButton} onClick={onClickHandler}>
+          <Image
+            src="/xmark.svg"
+            alt="close menu"
+            className={styles.menuLogo}
+            width={100}
+            height={24}
+            priority
+          ></Image>
+        </button>
+        <ul className={styles.menuList}>
+          <li>Über uns</li>
+          <li>Projekte</li>
+          <li>Veranstaltungen</li>
+          <li>Kontakt</li>
+          <li>Spenden</li>
+        </ul>
+      </div>
+    </>
   );
 }
