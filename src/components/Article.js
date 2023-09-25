cc04c09a57e8e3bfea9f9119bacf13cd04500d0b
Louis Weiser <louis.weiser98@gmail.com>
2023-09-25 18:54:45 +0200

    style introduction section

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,6 @@
 import styles from "./Article.module.css";
 
-export function stringArrayIntoJSX(arrayOfStrings) {
+function stringArrayIntoJSX(arrayOfStrings) {
   const convertedText = arrayOfStrings.map((text, index) => {
     return (
       <p className={styles.paragraph} key={index}>
@@ -11,25 +11,13 @@ export function stringArrayIntoJSX(arrayOfStrings) {
   return convertedText;
 }
 
-export default function Article({ title, text, introduction }) {
+export default function Article({ title, text }) {
   const articleText = stringArrayIntoJSX(text);
-
-  if (introduction) {
-    return (
-      <article>
-        <div className={styles["article-container"]}>
-          <h1>Warum ist das Thema Inklusion für uns so wichtig ?</h1>
-          <h1>Jeder Mensch ist einzigartig - </h1>
-          <h1>... voneinander lernen bringt uns in der Gemeinschaft weiter!</h1>
-        </div>
-      </article>
-    );
-  }
 
   return (
     <article className={styles.article}>
       <div className={styles["article-container"]}>
-        {title && <h2>{title}</h2>}
+        {title && <h2 className={styles.headline}>{title}</h2>}
         {articleText}
       </div>
     </article>
--- src/components/sections/Introduction.js
+++ src/components/sections/Introduction.js
@@ -1,16 +1,25 @@
 import ImageSlider from "@/components/ImageSlider";
-import Article from "../Article.js";
 import { useSectionRefs } from "@/pages/_app.js";
 
 import { introductionText } from "../../../public/content/aboutus.js";
+
+import styles from "./Introduction.module.css";
 
 export default function IntroductionContent() {
   const sectionRefs = useSectionRefs();
 
   return (
-    <section id="section1" ref={sectionRefs[0]}>
+    <section
+      id="section1"
+      ref={sectionRefs[0]}
+      className={styles["section-container"]}
+    >
       <ImageSlider />
-      <Article introduction={true} text={introductionText}></Article>
+      <article className={styles["article-container"]}>
+        <h1>Warum ist das Thema Inklusion für uns so wichtig ?</h1>
+        <h1>Jeder Mensch ist einzigartig - </h1>
+        <h1>... voneinander lernen bringt uns in der Gemeinschaft weiter!</h1>
+      </article>
     </section>
   );
 }
