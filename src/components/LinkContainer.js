2de8e8039b344f9cca7ba515ea2d4e8726490ac5
Louis Weiser <louis.weiser98@gmail.com>
2023-09-27 11:49:39 +0200

    add animation for linkcontainer component

--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,10 +1,37 @@
 import Image from "next/image";
+import { useRef, useEffect } from "react";
 
 import styles from "./LinkContainer.module.css";
 
 export default function ProjectLinkContainer({ project }) {
+  const containerRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting) {
+          entry.target.classList.add(styles.animate);
+        }
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className={styles["component-container"]}>
+    <div className={styles["component-container"]} ref={containerRef}>
       <div className={styles["image-container"]}>
         <Image
           src={"/images" + project.image}
