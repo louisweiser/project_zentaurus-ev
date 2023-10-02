2aa163a444009d64a318b805d48d5ee5bc7d6959
Louis Weiser <louis.weiser98@gmail.com>
2023-10-02 23:17:37 +0200

    improve article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,25 +1,30 @@
 import styles from "./Article.module.css";
 
-function stringArrayIntoJSX(arrayOfStrings) {
-  const convertedText = arrayOfStrings.map((text, index) => {
-    return (
-      <p className={styles.paragraph} key={index}>
-        {text}
-      </p>
-    );
-  });
-  return convertedText;
+function StringArrayIntoJSX(arrayOfStrings) {
+  return arrayOfStrings.map((text, index) => (
+    <p className={styles.paragraph} key={index}>
+      {text}
+    </p>
+  ));
 }
 
 export default function Article({ title, text, subtitle, colored }) {
-  const articleText = stringArrayIntoJSX(text);
+  const articleText = StringArrayIntoJSX(text);
 
   return (
-    <article className={`${styles.article} ${colored ? styles.color : ""}`}>
-      <div className={styles["article-container"]}>
-        {title && <h2 className={styles.title}>{title}</h2>}
-        {subtitle && <h3 className={styles.subtitle}>{subtitle}</h3>}
-        <div className={styles.text}>{articleText}</div>
+    <article
+      className={`${styles.article} ${colored ? styles.coloredBackground : ""}`}
+      aria-labelledby={title ? "article-title" : undefined}
+      aria-describedby="article-text"
+    >
+      {title && (
+        <h2 className={styles.title} id="article-title">
+          {title}
+        </h2>
+      )}
+      {subtitle && <h3 className={styles.subtitle}>{subtitle}</h3>}
+      <div className={styles.text} id="article-text">
+        {articleText}
       </div>
     </article>
   );
