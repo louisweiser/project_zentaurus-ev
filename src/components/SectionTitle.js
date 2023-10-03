a6dbbdb768cacc6d262d42c5a920e7b456ee55b5
Louis Weiser <louis.weiser98@gmail.com>
2023-10-03 10:28:16 +0200

    Refactor SectionTitle component and styles

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,5 +1,5 @@
 import styles from "./SectionTitle.module.css";
 
 export default function SectionTitle({ title }) {
-  return <h1 className={styles.title}>{title}</h1>;
+  return <h1 className={styles.sectionTitle}>{title}</h1>;
 }
