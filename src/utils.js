3f3a7ac0d124e1f2e853c4778b064c6457ad4fdb
Louis Weiser <louis.weiser98@gmail.com>
2023-10-03 10:50:36 +0200

    remove unused utils.js

--- src/utils.js
+++
@@ -1,6 +0,0 @@
-export function stringArrayIntoJSX(arrayOfStrings) {
-  const convertedText = arrayOfStrings.map((text, index) => {
-    return <p key={index}>{text}</p>;
-  });
-  return convertedText;
-}
