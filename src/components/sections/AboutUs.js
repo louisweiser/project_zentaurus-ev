18a94161760954bb459498288dd93ff46900b2cd
Louis Weiser <louis.weiser98@gmail.com>
2023-10-03 13:25:32 +0200

    Refactor aboutUs data structure

--- src/components/sections/AboutUs.js
+++ src/components/sections/AboutUs.js
@@ -1,14 +1,10 @@
 import { useState } from "react";
+
 import Article from "../Article.js";
 import TeamCard from "../TeamCard.js";
 import SectionTitle from "../SectionTitle.js";
 import { useSectionRefs } from "@/pages/_app.js";
-
-import {
-  philosophyText,
-  aboutUsText,
-  unionText,
-} from "../../../public/content/aboutus.js";
+import { aboutUsData } from "../../../public/content/aboutus.js";
 import { teamData } from "../../../public/content/team.js";
 
 import styles from "./AboutUs.module.css";
@@ -21,13 +17,14 @@ export default function AboutUsContent() {
   return (
     <section id="section1" ref={sectionRefs[1]}>
       <SectionTitle title="Über Uns" />
-      <Article text={aboutUsText} colored={true}></Article>
-      <Article subtitle="Der Verein" text={unionText} colored={true}></Article>
-      <Article
-        subtitle="Philosophie"
-        text={philosophyText}
-        colored={true}
-      ></Article>
+      {aboutUsData.map((article) => (
+        <Article
+          key={article.id}
+          subtitle={article.title}
+          text={article.text}
+          colored={true}
+        />
+      ))}
       <article>
         <h3 className={styles.subtitle}>Das Team</h3>
         <div className={styles.container}>
