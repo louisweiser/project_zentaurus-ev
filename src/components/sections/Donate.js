e65269a87d5e8148c4e987d74bf32dc8564da736
Louis Weiser <louis.weiser98@gmail.com>
2023-10-05 10:03:29 +0200

    desktop layout for donate section

--- src/components/sections/Donate.js
+++ src/components/sections/Donate.js
@@ -10,28 +10,30 @@ export default function DonateContent() {
   return (
     <section id="section5" ref={sectionRefs[5]}>
       <SectionTitle title="Spenden" />
-      <Article text={bankData.text} colored={true} />
-      <div className={styles["content__container"]}>
-        <ul>
-          <li className={styles["content__list-item"]}>Registergericht:</li>
-          <li className={styles["content__list-item"]}>{bankData.county}</li>
-        </ul>
-        <ul>
-          <li className={styles["content__list-item"]}>Registernummer:</li>
-          <li className={styles["content__list-item"]}>{bankData.number}</li>
-        </ul>
-        <ul>
-          <li className={styles["content__list-item"]}>Bank:</li>
-          <li className={styles["content__list-item"]}>{bankData.name}</li>
-        </ul>
-        <ul>
-          <li className={styles["content__list-item"]}>IBAN:</li>
-          <li className={styles["content__list-item"]}>{bankData.iban}</li>
-        </ul>
-        <ul>
-          <li className={styles["content__list-item"]}>BIC:</li>
-          <li className={styles["content__list-item"]}>{bankData.bic}</li>
-        </ul>
+      <div className={styles["grid-container"]}>
+        <Article text={bankData.text} colored={true} />
+        <div className={styles["content__container"]}>
+          <ul>
+            <li className={styles["content__list-item"]}>Registergericht:</li>
+            <li className={styles["content__list-item"]}>{bankData.county}</li>
+          </ul>
+          <ul>
+            <li className={styles["content__list-item"]}>Registernummer:</li>
+            <li className={styles["content__list-item"]}>{bankData.number}</li>
+          </ul>
+          <ul>
+            <li className={styles["content__list-item"]}>Bank:</li>
+            <li className={styles["content__list-item"]}>{bankData.name}</li>
+          </ul>
+          <ul>
+            <li className={styles["content__list-item"]}>IBAN:</li>
+            <li className={styles["content__list-item"]}>{bankData.iban}</li>
+          </ul>
+          <ul>
+            <li className={styles["content__list-item"]}>BIC:</li>
+            <li className={styles["content__list-item"]}>{bankData.bic}</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
