876e9a6f23ec63cd50d9affbbfa773fbd37dbed1
Louis Weiser <louis.weiser98@gmail.com>
2023-10-08 09:53:19 +0200

    add animation for title changing in Headline component

--- src/components/Header/Headline.js
+++ src/components/Header/Headline.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import styles from "./Headline.module.css";
 
@@ -14,36 +14,62 @@ const Headlines = [
 
 export default function Headline() {
   const [headline, setHeadline] = useState("");
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [timeoutId, setTimeoutId] = useState(null);
+  const nextHeadlineRef = useRef("");
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setHeadline(Headlines[0]);
-        return;
-      }
+  const handleScroll = () => {
+    let newHeadline = "";
 
+    if (window.scrollY === 0) {
+      newHeadline = Headlines[0];
+    } else {
       for (let i = 0; i < Headlines.length; i++) {
         const section = document.querySelector(`#section${i}`);
         if (section) {
           const rect = section.getBoundingClientRect();
           if (rect.top <= 0 && rect.bottom > 0) {
-            setHeadline(Headlines[i]);
+            newHeadline = Headlines[i];
             break;
           }
           if (i === 1 && rect.top > 0) {
-            setHeadline(Headlines[0]);
+            newHeadline = Headlines[0];
             break;
           }
         }
       }
-    };
+    }
 
+    if (newHeadline !== nextHeadlineRef.current) {
+      setIsAnimating(true);
+      nextHeadlineRef.current = newHeadline;
+
+      if (timeoutId) clearTimeout(timeoutId);
+
+      const id = setTimeout(() => {
+        setHeadline(nextHeadlineRef.current);
+        setIsAnimating(false);
+      }, 500);
+
+      setTimeoutId(id);
+    }
+  };
+
+  useEffect(() => {
     window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [headline, isAnimating, timeoutId]);
 
-  return <h1 className={styles["headline"]}>{headline}</h1>;
+  return (
+    <h1
+      className={`${styles["headline"]} ${
+        isAnimating ? styles["headline--fadeOut"] : styles["headline--fadeIn"]
+      }`}
+    >
+      {headline}
+    </h1>
+  );
 }
